08e725d8bd03b18ba4a55045632c780287865731
Yash Gattewar <yashgattewar@gmail.com>
2026-09-04 11:11:42 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (71.31%) - LeetSync

---
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+       List<String> ans= new ArrayList<>();
+       int j=0;
+       for(int i=1;i<=n&&j<target.length;i++){
+        ans.add("Push");
+         if(target[j]!=i){
+            ans.add("Pop");
+        }
+        if(target[j]==i){
+            j++;
+        }
+       
+       }
+    return ans;
+    }
+}
